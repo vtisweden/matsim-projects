ad1f91bf2e5f43178ca02d123cd45f82e9e08715
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2023-11-22 16:16:58 +0100

    adding utility class

---
+++ samgods/src/main/java/se/vti/samgods/legacy/SamgodsUtilities.java
@@ -0,0 +1,33 @@
+/**
+ * org.matsim.contrib.emulation
+ * 
+ * Copyright (C) 2023 by Gunnar Flötteröd (VTI, LiU).
+ * 
+ * VTI = Swedish National Road and Transport Institute
+ * LiU = Linköping University, Sweden
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>. See also COPYING and WARRANTY file.
+ */
+package se.vti.samgods.legacy;
+
+import org.matsim.api.core.v01.network.Node;
+
+public class SamgodsUtilities {
+
+	private SamgodsUtilities() {		
+	}
+	
+	public static Boolean isTransferPoint(final Node node) {
+		return (Boolean) node.getAttributes().getAttribute(SamgodsNetworkReader.NODE_IS_TERMINAL);
+	}
+	
+}
