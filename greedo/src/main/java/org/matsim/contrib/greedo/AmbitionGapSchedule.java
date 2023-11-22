2173d07a00ce2064708e0a9bc3c42897b3b73858
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2023-11-22 11:10:16 +0100

    exogeneous schedule on ambition level

--- greedo/src/main/java/org/matsim/contrib/greedo/AmbitionGapSchedule.java
+++ greedo/src/main/java/org/matsim/contrib/greedo/AmbitionGapSchedule.java
@@ -1,5 +1,5 @@
 /**
- * org.matsim.contrib.emulation
+ * org.matsim.contrib.greedo
  * 
  * Copyright (C) 2023 by Gunnar Flötteröd (VTI, LiU).
  * 
@@ -21,74 +21,79 @@ package org.matsim.contrib.greedo;
 
 import java.util.LinkedList;
 
-import floetteroed.utilities.math.BasicStatistics;
-
+/**
+ * 
+ * @author GunnarF
+ *
+ */
 class AmbitionGapSchedule {
 
+	// -------------------- CONSTANTS --------------------
+
 	private final int warmupIterations;
-	private final double iterationToLevelReductionExponent;
 
-	private final boolean strictReduce;
+	private final double iterationToLevelExponent;
 
-	private Double initialGap;
-	private Double lastEta;
+	// -------------------- MEMBERS --------------------
 
-	private final BasicStatistics initialStats = new BasicStatistics();
+	private Double initialGap = null;
+
 	private final LinkedList<Double> gaps = new LinkedList<>();
 
-	AmbitionGapSchedule(final int warmupIterations, final double iterationToLevelReductionExponent,
-			final boolean strictReduce) {
+	// -------------------- CONSTRUCTION --------------------
+
+	AmbitionGapSchedule(final int warmupIterations, final double iterationToLevelReductionExponent) {
+		if (warmupIterations < 2) {
+			throw new IllegalArgumentException("warmupIterations must be >= 2");
+		}
+		if (iterationToLevelReductionExponent > 0) {
+			throw new IllegalArgumentException("iterationToLevelReductionException must be non-positive");
+		}
 		this.warmupIterations = warmupIterations;
-		this.iterationToLevelReductionExponent = iterationToLevelReductionExponent;
-		this.strictReduce = strictReduce;
+		this.iterationToLevelExponent = iterationToLevelReductionExponent;
 	}
 
+	// -------------------- IMPLEMENTATION --------------------
+
 	void registerGap(final double gap) {
-		if (this.initialStats.size() < this.warmupIterations) {
-			this.initialStats.add(gap);
-		}
 		this.gaps.addFirst(gap);
 	}
 
 	double getEta(final int iteration, final boolean constrain) {
-		final double etaMSA = Math.pow(1 + iteration, this.iterationToLevelReductionExponent);
-		if (iteration < 2 * this.warmupIterations) {
-			this.lastEta = etaMSA;
+		final double etaMSA = Math.pow(1 + iteration, this.iterationToLevelExponent);
+		final double eta;
+		if (iteration < this.warmupIterations) {
+			eta = etaMSA;
 		} else {
 			final double meanGap = this.gaps.subList(0, iteration / 2).stream().mapToDouble(g -> g).average()
 					.getAsDouble();
 			if (this.initialGap == null) {
+				assert (iteration == 2 * this.warmupIterations);
 				this.initialGap = meanGap;
 			}
-			final double newEta = this.initialGap / meanGap
-					* Math.pow(0.75 * (1.0 + iteration), this.iterationToLevelReductionExponent);
-			if (this.strictReduce && (this.lastEta != null)) {
-				this.lastEta = Math.min(this.lastEta, newEta);
-			} else {
-				this.lastEta = newEta;
-			}
+			eta = Math.pow(1.0 + iteration, this.iterationToLevelExponent) * (this.initialGap / meanGap);
 		}
 		if (constrain) {
-			return Math.max(0.0, Math.min(1.0, this.lastEta));
+			return Math.max(0.0, Math.min(1.0, eta));
 		} else {
-			return this.lastEta;
+			return eta;
 		}
 	}
 
+	// -------------------- MAIN-FUNCTION, ONLY FOR TESTING --------------------
+
 	public static void main(String[] args) {
 
-		AmbitionGapSchedule ags = new AmbitionGapSchedule(10, -1.0, false);
-		AmbitionGapSchedule agsStrict = new AmbitionGapSchedule(10, -1.0, true);
+		AmbitionGapSchedule ags = new AmbitionGapSchedule(10, -0.5);
 
-		System.out.println("gap\teta\tetastrict");
+		System.out.println("gap\teta");
 		for (int it = 0; it < 1000; it++) {
-			double gap = 200.0 * Math.pow(1 + it, -0.5);
+			double gap = 200.0 * Math.pow(1 + it, -0.25);
 			gap += 0.2 * (Math.random() - 0.5) * gap;
 
 			ags.registerGap(gap);
-			agsStrict.registerGap(gap);
 
-			System.out.println(gap + "\t" + ags.getEta(it, false) + "\t" + agsStrict.getEta(it, false));
+			System.out.println(gap + "\t" + ags.getEta(it, false));
 
 		}
 
