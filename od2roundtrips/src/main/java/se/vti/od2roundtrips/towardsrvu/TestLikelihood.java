a00cbdf67fc31d63c1db9edbcfd828010907f335
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2024-09-09 17:59:10 +0200

    example for michael

---
+++ od2roundtrips/src/main/java/se/vti/od2roundtrips/towardsrvu/TestLikelihood.java
@@ -0,0 +1,38 @@
+/**
+ * se.vti.od2roundtrips.towardsrvu
+ * 
+ * Copyright (C) 2024 by Gunnar Flötteröd (VTI, LiU).
+ * 
+ * VTI = Swedish National Road and Transport Institute
+ * LiU = Linköping University, Sweden
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>. See also COPYING and WARRANTY file.
+ */
+package se.vti.od2roundtrips.towardsrvu;
+
+import se.vti.roundtrips.multiple.MultiRoundTrip;
+import se.vti.roundtrips.preferences.PreferenceComponent;
+import se.vti.roundtrips.single.RoundTrip;
+
+public class TestLikelihood extends PreferenceComponent<MultiRoundTrip<?>> {
+
+	@Override
+	public double logWeight(MultiRoundTrip<?> multiRoundTrip) {
+		double sum = 0.0;
+		for (RoundTrip<?> roundTrip : multiRoundTrip) {
+			Sociodemographics sociodem = (Sociodemographics) roundTrip.getAttributes();
+			// do smth to sum
+		}
+		return sum;
+	}
+
+}
